cd15bef4285fa66c4b7a453b9c696b3ee61a59ed
Mariana Torres Torres <mari.torret@gmail.com>
2021-02-26 02:25:10 +0000

    make few styling adjustments, typewriter functionality works(ish)

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,18 +11,57 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/*let i = 0;
-function typeWriter() {
-  const facts =
-      ["I'm a Digital Arts and Sciences Engineering student", 
-      "I'm a Full-Stack Developer",
-      "I'm a Scrum Master", 
-      "I'm in Google SPS >:)"];
 
-  const fact = facts[Math.floor(Math.random() * facts.length)];
-  if (i < fact.length) {
-    //document.getElementById("typewriter").innerHTML += fact.charAt(i);
-    //i++;
-    //setTimeout(typeWriter, 50);
+let i = 0;
+let facts = [
+  "Digital Arts and Sciences Engineer ",
+  "Full-Stack Developer ",
+  "Scrum Master ",
+  "Google SPS participant! >:) "
+];
+let txt = "I'm a " + facts[0];
+let speed = 50;
+let backspace = false;
+
+window.onload = function() {
+  typeWriter();
+  i = 0;
+  txt = "I'm a " + facts[Math.floor(Math.random() * facts.length)];
+  backspace = false;
+};
+
+document.addEventListener("click", function() {
+  typeWriter();
+  i = 0;
+  txt = "I'm a " + facts[Math.floor(Math.random() * facts.length)];
+  backspace = false;
+});
+
+function typeWriter() {
+  if (i <= txt.length && !backspace) {
+    document.getElementById("typed").innerHTML = txt.substring(0, i) + "|";
+    i++;
+    if (i === txt.length + 1) {
+      backspace = true;
+      setTimeout(wait(), 15000);
+    }
+    setTimeout(typeWriter, speed);
   }
-}*/
+  if (backspace) {
+    if (i >= 0) {
+      document.getElementById("typed").innerHTML = txt.substring(0, i) + "|";
+      i--;
+      setTimeout(typeWriter, speed + 10);
+    }
+  }
+}
+
+function wait() {
+  for (let num = 0; num < 100; num++) {
+    if (num % 2 === 0) {
+      document.getElementById("typed").innerHTML = txt + "|";
+    } else {
+      document.getElementById("typed").innerHTML = txt;
+    }
+  }
+}
