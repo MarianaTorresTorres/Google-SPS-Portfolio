8624857b1f0b67ddec03899a064f76913234c6c7
Mariana Torres Torres <mari.torret@gmail.com>
2021-03-16 19:12:25 +0000

    datastore is working correctly and displaying all comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,17 +1,3 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 let i = 0;
 let txt = "I'm a ";
 let speed = 75;
@@ -22,6 +8,21 @@ window.onload = function() {
   fetchText(0);
   typeFact();
 };
+
+/*Fetches comments from the server and adds them to the DOM. */
+function loadComments() {
+  fetch('/list-comments').then(response => response.json()).then((comments) => {
+    const commentListElement = document.getElementById('comment-list');
+    comments.forEach(function(comment) {
+        const commentElement = document.createElement('li');
+        commentElement.className = 'comment';
+        const textElement = document.createElement('span');
+        textElement.innerText = comment.text;   
+        commentElement.appendChild(textElement);
+        commentListElement.appendChild(commentElement);
+    })
+  });
+}
 
 async function fetchText(index) {
     const responseFromServer = await fetch('/servlet');
